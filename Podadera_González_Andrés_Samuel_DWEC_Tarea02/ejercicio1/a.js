3752aa59e02b95744bb20d23a06b64eb9c6ebd64
Andres <andrespodadera87@gmail.com>
2023-11-01 16:01:45 +0100

    adding ejercicio 5

--- Podadera_González_Andrés_Samuel_DWEC_Tarea02/ejercicio1/a.js
+++ Podadera_González_Andrés_Samuel_DWEC_Tarea02/ejercicio1/a.js
@@ -1,13 +1,17 @@
+/*Ejercicio 1: Adivina el futuro.  brujIA.*/
+/*En esta tarea vamos a crear un software adivinatorio brujIA que responda a nuestras preguntas.  Lo primero es que le hagamos una pregunta. A esa pregunta el software brujIA va a lanzar los dados. Si el número es superior a 0.499999 entonces la respuesta es sí. Si la pregunta contiene "DWEC" la brujIA se enfada.*/
+
 const divRespuestas = document.getElementsByClassName("respuestas");
 let pregunta = "";
 let salir = false;
+let numeroRandom;
 do {
     pregunta = prompt("Lanza tu pregunta");
-    const numeroRandom = Math.random();
-    if (pregunta.includes("DWEC")) {
+    numeroRandom = Math.random();
+    if (pregunta.toLocaleLowerCase().includes("dwec")) {
         divRespuestas[0].innerHTML += `<p>Yo: ${pregunta}.brujIA: A esas preguntas no respondo.Adiós.</p>`;
         salir = true;
-    } else if (numeroRandom < 0.499999) {
+    } else if (numeroRandom > 0.499999) {
         divRespuestas[0].innerHTML += `<p>Yo: ${pregunta}.brujIA: Sí.</p>`;
     } else {
         divRespuestas[0].innerHTML += `<p>Yo: ${pregunta}.brujIA: No.</p>`;
