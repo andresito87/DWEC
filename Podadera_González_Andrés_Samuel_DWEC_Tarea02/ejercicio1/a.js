63c076e58329d79a48a3fce30c80abcce74e341c
Andres <andrespodadera87@gmail.com>
2023-11-01 12:42:21 +0100

    modificando ejercicio 1

--- Podadera_González_Andrés_Samuel_DWEC_Tarea02/ejercicio1/a.js
+++ Podadera_González_Andrés_Samuel_DWEC_Tarea02/ejercicio1/a.js
@@ -1,13 +1,15 @@
 const divRespuestas = document.getElementsByClassName("respuestas");
 let pregunta = "";
+let salir = false;
 do {
     pregunta = prompt("Lanza tu pregunta");
     const numeroRandom = Math.random();
-    if (numeroRandom < 0.499999) {
+    if (pregunta.includes("DWEC")) {
+        divRespuestas[0].innerHTML += `<p>Yo: ${pregunta}.brujIA: A esas preguntas no respondo.Adiós.</p>`;
+        salir = true;
+    } else if (numeroRandom < 0.499999) {
         divRespuestas[0].innerHTML += `<p>Yo: ${pregunta}.brujIA: Sí.</p>`;
     } else {
         divRespuestas[0].innerHTML += `<p>Yo: ${pregunta}.brujIA: No.</p>`;
     }
-} while (!pregunta.includes("DWEC"));
-
-divRespuestas[0].innerHTML += `<p>Yo: ${pregunta}.brujIA: A esas preguntas no respondo.Adiós.</p>`;
+} while (!salir);
