b5e64fa1e8e37789d629924b8017d7adb6508735
Andres <andrespodadera87@gmail.com>
2024-05-23 20:54:26 +0200

    terminado ejercicio 1

--- ExamenJunioDWEC/Ejercicio Repaso Examen/index.js
+++ ExamenJunioDWEC/Ejercicio Repaso Examen/index.js
@@ -1,5 +1,123 @@
-// Ejercicio 2: Quokkas en adopción.
-/*
+document.addEventListener('DOMContentLoaded', () => {
+  /*  Ejercicio 1: Formulario de adopción de quokkas.
+  Tu misión es proteger de la extinción a los quokkas. 
+
+Para ello vamos a crear un formulario de se va a realizar un formulario de adopción de Quokkas (utilizando DOM o jQuery). Deberéis contemplar los siguientes puntos:
+
+Un primer campo que es el nombre del quokka. No se especifica la longitud. Va a estar compuesto de dos palabras separadas por un espacio a tres máximo. Los caracteres válidos para el nombre serán alfabéticos latinos desde «a» a la «z» mayúsculas o minúsculas SIN ACENTOS. La segunda palabra debe comenzar en mayúscula.
+
+Perry Come → Correcto.
+
+Perry   come→ Incorrecto. Aunque tiene tres espacios máximo la segunda es mayúscula.
+
+elvis Quokka → Correcto. No es obligatorio que empiece la primera palabra en mayúscula.
+
+Reggaeton. → Incorrecta. Una sola palabra y a los quokkas les mola el rock y Madonna.
+
+Un segundo campo de texto, humano adoptador, va a ser un código de adopción (password) con las siguientes condiciones:
+
+- La última letra debe estar en minúscula.
+
+- Debe aparecer “,” obligatoriamente. No hay límite.
+
+- No puede contener la palabra "extinción" (sin acento).
+
+- Debe comenzar por * y la longitud máxima es de 20.
+
+- No pueden aparecer dígitos. El resto de los caracteres está permitido.
+
+Un tercer campo de texto, donación anual, debe ser un número entre 0 y 1000. La ,  (coma) va a ser el separador y no el punto.
+
+Un botón enviar que muestra un div con id=”resultado” el contenido del formulario sin enviarse realmente. Al enviar correctamente se debe mostrar la foto del animal seleccionado, el nombre del quokka, los datos del humano. Debe ser elegido entre varias fotos que se adjuntan al examen. Esa elección será al azar entre el total.
+
+Los mensajes de error deben ser creados con DOM.  O sea creas un párrafo, con negr
+
+<p> contenido  texto indicando corrección o error <b> contenido campo error  o éxito </b> </p>
+
+Deberéis cambiar el color de fondo a amarillo si ha habido un error en ese campo.
+
+Nota: No se puede utilizar pattern. Se pueden utilizar regex101 y páginas similares (pero preguntándome antes). La elegancia del código también está en esos puntos. Primero que funcione. Puedes hacer otra solución con pattern para practicar.*/
+
+  const botonEnviarFormulario = document.getElementById(
+    'botonEnviarFormulario'
+  );
+
+  botonEnviarFormulario.addEventListener('click', event => {
+    event.preventDefault();
+    // eliminar el contenido de resultado
+    // document.getElementById('resultado').innerHTML = '';
+    while (document.getElementById('resultado').hasChildNodes()) {
+      document
+        .getElementById('resultado')
+        .removeChild(document.getElementById('resultado').firstChild);
+    }
+    const nombre = document.getElementById('nombre').value;
+    const humanoAdoptador = document.getElementById('humanoAdoptador').value;
+    const donacionAnual = document.getElementById('donacionAnual').value;
+
+    const patronNombre = /[A-Z]?[a-z]+ [A-Z][a-z]+( [A-Z][a-z]+)?/;
+    const patronHumanoAdoptador = /^\*[^0-9extincion]{0,18},[^0-9]*[a-z]$/;
+    const patronDonacionAnual = /^(0|[1-9]\d{0,2})(,\d{1,2})?$/;
+
+    if (!patronNombre.test(nombre)) {
+      document.getElementById('nombre').style.backgroundColor = 'yellow';
+      const parrafoNombre = document.createElement('p');
+      parrafoNombre.textContent = `Nombre incorrecto: ${nombre}`;
+      document.getElementById('resultado').appendChild(parrafoNombre);
+    } else {
+      document.getElementById('nombre').style.backgroundColor = 'white';
+    }
+
+    if (!patronHumanoAdoptador.test(humanoAdoptador)) {
+      document.getElementById('humanoAdoptador').style.backgroundColor =
+        'yellow';
+      const parrafoHumanoAdoptador = document.createElement('p');
+      parrafoHumanoAdoptador.textContent = `Código de adopción incorrecto: ${humanoAdoptador}`;
+      document.getElementById('resultado').appendChild(parrafoHumanoAdoptador);
+    } else {
+      document.getElementById('humanoAdoptador').style.backgroundColor =
+        'white';
+    }
+
+    if (!patronDonacionAnual.test(donacionAnual)) {
+      document.getElementById('donacionAnual').style.backgroundColor = 'yellow';
+      const parrafoDonacionAnual = document.createElement('p');
+      parrafoDonacionAnual.textContent = `Donación anual incorrecta: ${donacionAnual}`;
+      document.getElementById('resultado').appendChild(parrafoDonacionAnual);
+    } else {
+      document.getElementById('donacionAnual').style.backgroundColor = 'white';
+    }
+
+    if (
+      patronNombre.test(nombre) &&
+      patronHumanoAdoptador.test(humanoAdoptador) &&
+      patronDonacionAnual.test(donacionAnual)
+    ) {
+      // añadir la imagen
+      const imagen = document.createElement('img');
+      // elegir una imagen al azar
+      const numeroImagen = Math.floor(Math.random() * 6) + 1;
+      imagen.src = `./imagenes/${numeroImagen}.jpeg`;
+      document.getElementById('resultado').appendChild(imagen);
+
+      // añadir el nombre del quokka
+      const parrafoNombre = document.createElement('p');
+      parrafoNombre.textContent = `Nombre: ${nombre}`;
+      document.getElementById('resultado').appendChild(parrafoNombre);
+
+      // añadir los datos del humano
+      const parrafoHumanoAdoptador = document.createElement('p');
+      parrafoHumanoAdoptador.textContent = `Código de adopción: ${humanoAdoptador}`;
+      document.getElementById('resultado').appendChild(parrafoHumanoAdoptador);
+
+      const parrafoDonacionAnual = document.createElement('p');
+      parrafoDonacionAnual.textContent = `Donación anual: ${donacionAnual}`;
+      document.getElementById('resultado').appendChild(parrafoDonacionAnual);
+    }
+  });
+
+  // Ejercicio 2: Quokkas en adopción.
+  /*
 Realiza el examen dos veces, una con jQuery y otra con  fetch.
 
 Se va a obtener la siguiente información (calculando):
@@ -13,10 +131,9 @@ El alimento que más gusta es: “Hierbas Frescas”.
 Hay 4 saltadores profesionales.
 */
 
-// ************ FETCH ************ //
-const divContenidoFetch = document.getElementById('contenidoFetch');
+  // ************ FETCH ************ //
+  const divContenidoFetch = document.getElementById('contenidoFetch');
 
-document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('botonFetch').addEventListener('click', () => {
     // eliminar el contenido anterior sin usar innerHTML
     // divContenidoFetch.innerHTML = '';
